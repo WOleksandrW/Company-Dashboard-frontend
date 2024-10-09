dcfd87b75d5afb6e257e3b4aa2562731281fce75
WOleksandrW <oleks.221.39@gmail.com>
2024-10-09 11:22:20 +0000

    feat: add react-icons and add view btn to Input type = password

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,7 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { UseFormRegisterReturn } from 'react-hook-form';
+
+import { IoEyeOffSharp, IoEyeSharp } from 'react-icons/io5';
 
 import styles from './Input.module.scss';
 
@@ -12,14 +14,36 @@ interface IProps {
   register?: UseFormRegisterReturn;
 }
 
-function Input({ className, register, ...rest }: IProps) {
+function Input({ className, register, type, ...rest }: IProps) {
+  const [isVisible, setIsVisible] = useState(false);
+
   const classNames = useMemo(() => {
     let str = styles['input'];
     if (className) str += ` ${className}`;
     return str;
   }, [className]);
 
-  return <input className={classNames} {...rest} {...register} />;
+  if (type === 'password') {
+    return (
+      <div className={styles['input-block']}>
+        <input
+          className={classNames}
+          type={isVisible ? 'text' : 'password'}
+          {...rest}
+          {...register}
+          autoComplete="off"
+        />
+        <button
+          className={styles['eye-btn']}
+          type="button"
+          onClick={() => setIsVisible((prev) => !prev)}>
+          {isVisible ? <IoEyeSharp /> : <IoEyeOffSharp />}
+        </button>
+      </div>
+    );
+  }
+
+  return <input className={classNames} type={type} {...rest} {...register} />;
 }
 
 export default Input;
