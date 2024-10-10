b4dbb8948a4eeb4e4755a5f9eeac2a0033943487
WOleksandrW <oleks.221.39@gmail.com>
2024-10-10 11:57:31 +0000

    feat: add material-ui to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { ToastContainer } from 'react-toastify';
 import Routing from './routing/Routing';
 import { onAxiosError } from './constants/handlers';
@@ -20,12 +21,34 @@ const queryClient = new QueryClient({
   }
 });
 
+const theme = createTheme({
+  typography: {
+    h1: {
+      fontSize: '3.2rem'
+    },
+    h2: {
+      fontSize: '2.4rem'
+    },
+    h6: {
+      fontSize: '1.8rem'
+    },
+    body1: {
+      fontSize: '1.6rem'
+    },
+    body2: {
+      fontSize: '1.4rem'
+    }
+  }
+});
+
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
   return (
     <QueryClientProvider client={queryClient}>
-      {isLoading ? <AuthController setIsLoading={setIsLoading} /> : <Routing />}
+      <ThemeProvider theme={theme}>
+        {isLoading ? <AuthController setIsLoading={setIsLoading} /> : <Routing />}
+      </ThemeProvider>
       <ToastContainer style={{ fontSize: '16px' }} closeOnClick />
     </QueryClientProvider>
   );
