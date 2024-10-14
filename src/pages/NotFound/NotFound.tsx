07aee55056f519441ecdec2a34053a2bdd4c653f
WOleksandrW <oleks.221.39@gmail.com>
2024-10-14 17:32:12 +0000

    feat: add content to NotFound page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,38 @@
+import { Box, Button, Typography } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+
 function NotFound() {
-  return <p className="p1">Not Found</p>;
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        textAlign: 'center',
+        padding: 2
+      }}>
+      <Typography typography="h1" sx={{ fontSize: { xs: '4rem', md: '6rem' }, fontWeight: 'bold' }}>
+        404
+      </Typography>
+      <Typography typography="h4" sx={{ marginBottom: 2 }}>
+        Sorry, the page you are looking for does not exist.
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        component={NavLink}
+        to="/"
+        sx={{
+          typography: 'body1',
+          marginTop: 2,
+          padding: '10px 20px'
+        }}>
+        Go Back to Home
+      </Button>
+    </Box>
+  );
 }
 
 export default NotFound;
