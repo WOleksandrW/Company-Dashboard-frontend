85fc210d705918eecbfe524a9d44c6a77c6581cf
WOleksandrW <oleks.221.39@gmail.com>
2024-10-17 08:35:27 +0000

    fix: replace content of PopupCreateAdmin component

--- src/pages/Dashboard/components/PopupCreateAdmin/PopupCreateAdmin.tsx
+++ src/pages/Dashboard/components/PopupCreateAdmin/PopupCreateAdmin.tsx
@@ -1,16 +1,13 @@
 import { useCallback, useEffect } from 'react';
 import { useMutation, useQueryClient } from 'react-query';
-import { Box, Button, Typography } from '@mui/material';
-import { Controller, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import api from '../../../../api';
 import { toast } from 'react-toastify';
-import { ModalUsage, TextFieldUsage } from '../../../../components';
+import { FormModalUsage } from '../../../../components';
 import { EQueryKeys, ERole } from '../../../../types/enums';
 import { schemaSignUp } from '../../../../types/schema';
 import { TSignUpBody } from '../../../../types/types';
-
-import { MdCancel, MdOutlineDone } from 'react-icons/md';
 
 interface ICreateAdminForm {
   username: string;
@@ -65,90 +62,40 @@ function PopupCreateAdmin({ open, setOpen }: IProps) {
   }, [open]);
 
   return (
-    <ModalUsage open={open} setOpen={setOpen}>
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
-        <Typography variant="h2" className="primary-color" sx={{ fontWeight: 'bold' }}>
-          Create admin
-        </Typography>
-        <Box
-          component="form"
-          onSubmit={(e) => {
-            handleSubmit(onSubmit)(e);
-          }}
-          sx={{ display: 'flex', flexDirection: 'column', gap: '32px' }}>
-          <Box
-            sx={{
-              width: 'min(300px, 100%)',
-              display: 'flex',
-              flexDirection: 'column',
-              gap: '16px',
-              alignSelf: 'center'
-            }}>
-            <Controller
-              name="username"
-              control={control}
-              render={({ field }) => (
-                <TextFieldUsage
-                  label="Username"
-                  errorMessage={errors.username?.message}
-                  field={field}
-                />
-              )}
-            />
-            <Controller
-              name="email"
-              control={control}
-              render={({ field }) => (
-                <TextFieldUsage label="Email" errorMessage={errors.email?.message} field={field} />
-              )}
-            />
-            <Controller
-              name="password"
-              control={control}
-              render={({ field }) => (
-                <TextFieldUsage
-                  label="Password"
-                  type="password"
-                  autoComplete="off"
-                  errorMessage={errors.password?.message}
-                  field={field}
-                />
-              )}
-            />
-            <Controller
-              name="confirm"
-              control={control}
-              render={({ field }) => (
-                <TextFieldUsage
-                  label="Confirm"
-                  type="password"
-                  autoComplete="off"
-                  errorMessage={errors.confirm?.message}
-                  field={field}
-                />
-              )}
-            />
-          </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Button
-              sx={{ typography: 'body1' }}
-              variant="outlined"
-              type="button"
-              startIcon={<MdCancel />}
-              onClick={() => setOpen(false)}>
-              Cancel
-            </Button>
-            <Button
-              sx={{ typography: 'body1' }}
-              variant="outlined"
-              type="submit"
-              startIcon={<MdOutlineDone />}>
-              Create
-            </Button>
-          </Box>
-        </Box>
-      </Box>
-    </ModalUsage>
+    <FormModalUsage
+      open={open}
+      setOpen={setOpen}
+      title="Create Admin"
+      inputs={[
+        {
+          controlParams: { control, name: 'username' },
+          label: 'Username',
+          errorMessage: errors.username?.message
+        },
+        {
+          controlParams: { control, name: 'email' },
+          label: 'Email',
+          errorMessage: errors.email?.message
+        },
+        {
+          controlParams: { control, name: 'password' },
+          label: 'Password',
+          type: 'password',
+          autoComplete: 'off',
+          errorMessage: errors.password?.message
+        },
+        {
+          controlParams: { control, name: 'confirm' },
+          label: 'Confirm password',
+          type: 'password',
+          autoComplete: 'off',
+          errorMessage: errors.confirm?.message
+        }
+      ]}
+      submitHandler={(e) => {
+        handleSubmit(onSubmit)(e);
+      }}
+    />
   );
 }
 
